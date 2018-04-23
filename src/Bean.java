9eb3b698a1177db87d8ad78ae6628a4c20f4aa31
liuzx8888 <liuzx8888@yeah.net>
2018-04-24 00:09:01 +0800

    Java Bean

---
+++ src/Bean.java
@@ -0,0 +1,14 @@
+
+public class Bean {
+	
+	public static void main (String []args) {
+		Person person = new Person("liuzx", 28);
+		Book book = new Book("梦里花落知多少", "88");
+		person.setBook(book);
+		book.setPerson(person);
+		System.out.println(person.getName() +  person.getAge() + person.getBook().getName());
+		System.out.println(book.getName() + book.getPrice() +book.getPerson().getName());
+		
+	}
+
+}
