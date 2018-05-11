30ef1a8eaca22e8b6f03d273fd3188853efeb0f5
Administrator <Administrator@Liuzx>
2018-05-11 17:47:36 +0800

    instanceof

--- com.liuzx.Demo/TestManyType/TestType.java
+++ com.liuzx.Demo/TestManyType/TestType.java
@@ -21,7 +21,7 @@ public  static void main (String[] agrs) {
 	 *：父类不知道子类的类型，所以转换的时候报错
 	 * 所以需要先把 B类向上转型，跟A类建立好关系后，再向下转型成B类才不报错
 	 * 
-	 * 
+	 *
 	 * */
       B b1 = (B) a2;
 	
@@ -43,5 +43,8 @@ public  static void main (String[] agrs) {
     System.out.println(b.show(b));      /*  :  B AND B*/    
     System.out.println(b.show(c));      /*  : B类没有 show(c)方法，父类A 也没有该方法，C继承 B，转换成B，：B AND B */
     System.out.println(b.show(d));      /* B类没有show(d),到父类中寻找对应 show(c)方法，父类中存在，：A:D */  
+	 
+    System.out.println(a2 instanceof A);
+    System.out.println(a2 instanceof B); 
 	}
 }
