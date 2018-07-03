aa1ee1b285ac15fb0cf63e9f7c92444e9e096bd7
Administrator <Administrator@Liuzx>
2018-07-03 14:01:25 +0800

    Jdbc

---
+++ com.liuzx.Demo/jdbcdemo/TestCaseDemo1.java
@@ -0,0 +1,19 @@
+package jdbcdemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCaseDemo1 {
+
+	@Test
+	public void testConnectUtil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetConnect() {
+		fail("Not yet implemented");
+	}
+
+}
